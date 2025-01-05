b217095e11576dab90fab52453dbb684c6e8964e
Eunji Kim <but05051@gmail.com>
2025-01-05 20:58:53 +1100

    day-5

---
+++ Day-4/coin-tracker/src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </StrictMode>
+);
