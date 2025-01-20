033107f06db415c55032fb8924054b49721bc60a
Eunji Kim <but05051@gmail.com>
2025-01-20 22:26:11 +1100

    #6.16

--- Day-4/to-do/src/components/ToDoList.tsx
+++ Day-4/to-do/src/components/ToDoList.tsx
@@ -1,20 +1,37 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "./atoms";
+import { toDoSelector, toDoState } from "./atoms";
 import ToDo from "./ToDo";
 import { todo } from "node:test";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo></CreateToDo>
+      <h2>To do</h2>
+      <hr />
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>
+
+      <h2>Doing</h2>
+      <hr />
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo}></ToDo>
+        ))}
+      </ul>
+
+      <h2>Done</h2>
+      <hr />
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo}></ToDo>
         ))}
       </ul>
