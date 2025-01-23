79de9edd3e2e0e7b1da9d14b650c3f6deb6ba231
Eunji Kim <but05051@gmail.com>
2025-01-23 17:08:42 +1100

    #7.1

--- Day-4/trello/src/App.tsx
+++ Day-4/trello/src/App.tsx
@@ -69,9 +69,12 @@ a {
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <>
@@ -82,7 +85,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </>
   );
 }
