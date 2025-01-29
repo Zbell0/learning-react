9da25a9cbc7bcd2932ce4335d6768c364d182a29
Eunji Kim <but05051@gmail.com>
2025-01-29 22:57:32 +1100

    #7.13

--- Day-4/trello/src/components/Board.tsx
+++ Day-4/trello/src/components/Board.tsx
@@ -1,10 +1,11 @@
 import { Droppable } from "react-beautiful-dnd";
 import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
-  padding: 20px 10px;
+  padding: 10px 0px;
   border-radius: 5px;
   min-height: 200px;
   display: flex;
@@ -30,18 +31,28 @@ interface IAreaProps {
 const Area = styled.div<IAreaProps>`
   background-color: ${(props) =>
     props.isDraggingOver
-      ? "pink"
+      ? "#48A6A7"
       : props.draggingFromThisWith
-      ? "red"
-      : "blue"};
+      ? "#b6e5ea"
+      : props.theme.boardColor};
   flex-grow: 1;
   transition: background-color 0.3s ease-in-out;
+  padding: 20px 20px;
 `;
 
 function Board({ toDos, boardId }: IBoardProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = () => {
+    inputRef.current?.focus();
+    setTimeout(() => {
+      inputRef.current?.blur();
+    }, 5000);
+  };
   return (
     <Wrapper>
       <Title>{boardId}</Title>
+      <input ref={inputRef} placeholder="grab me"></input>
+      <button onClick={onClick}>Click me</button>
       <Droppable droppableId={boardId}>
         {(magic, info) => (
           <Area
--- Day-4/trello/src/components/DraggableCard.tsx
+++ Day-4/trello/src/components/DraggableCard.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   padding: 5px 10px;
   border-radius: 5px;
-  background-color: ${(props) => props.theme.cardColor};
   margin-bottom: 5px;
+  background-color: ${(props) =>
+    props.isDragging ? "#F2EFE7" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 5px rgba(0,0,0,0.3)" : "none"};
 `;
 
 interface IDdraggableProps {
@@ -17,8 +20,9 @@ interface IDdraggableProps {
 function DraggableCard({ toDo, index }: IDdraggableProps) {
   return (
     <Draggable key={toDo} draggableId={toDo} index={index}>
-      {(magic) => (
+      {(magic, snapshot) => (
         <Card
+          isDragging={snapshot.isDragging}
           ref={magic.innerRef}
           {...magic.draggableProps}
           {...magic.dragHandleProps}
