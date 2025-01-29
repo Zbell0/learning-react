42b7bd393a57acd436edb2e3ebb277bd1ee8ddcc
Eunji Kim <but05051@gmail.com>
2025-01-29 22:18:57 +1100

    #7.11

--- Day-4/trello/src/components/Board.tsx
+++ Day-4/trello/src/components/Board.tsx
@@ -7,6 +7,8 @@ const Wrapper = styled.div`
   padding: 20px 10px;
   border-radius: 5px;
   min-height: 200px;
+  display: flex;
+  flex-direction: column;
 `;
 
 interface IBoardProps {
@@ -20,13 +22,34 @@ const Title = styled.div`
   font-weight: 600;
   font-size: 18px;
 `;
+
+interface IAreaProps {
+  isDraggingOver: boolean;
+  draggingFromThisWith: boolean;
+}
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "pink"
+      : props.draggingFromThisWith
+      ? "red"
+      : "blue"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+`;
+
 function Board({ toDos, boardId }: IBoardProps) {
   return (
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, info) => (
+          <Area
+            isDraggingOver={info.isDraggingOver}
+            draggingFromThisWith={Boolean(info.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
             {toDos.map((toDo, index) => (
               <DraggableCard
                 key={toDo}
@@ -35,7 +58,7 @@ function Board({ toDos, boardId }: IBoardProps) {
               ></DraggableCard>
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
